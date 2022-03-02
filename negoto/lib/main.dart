8dde8bf6c746a9e4d037d4b75a4707d789185169
yugo nakamura <yugo.nakamura.0101@gmail.com>
2022-03-03 01:18:02 +0900

    firebase追加

--- negoto/lib/main.dart
+++ negoto/lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:negoto/pages/MainPage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
